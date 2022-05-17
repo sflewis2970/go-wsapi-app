86b2a0729dc60dd0849af56098d2d8185b9419fa
Shawn F Lewis <44281309+sflewis2970@users.noreply.github.com>
2022-05-17 15:40:01 -0700

    Update controllers.go
    * Refactored the response struct so that each request request (Quote and Dictionary) would have it's own response struct..
    * Moved the code specific to handling each request into it's own method.

--- cmd/wsapi/controllers/controllers.go
+++ cmd/wsapi/controllers/controllers.go
@@ -14,86 +14,133 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
-type ClientRequest struct {
+type WSRequest struct {
 	Request  string `json:"request"`
 	Category string `json:"category"`
 	Limit    string `json:"limit"`
 	Word     string `json:"word"`
 }
 
-type ServerResponse struct {
+type WSQuoteResponse struct {
 	Request  string `json:"request"`
 	Quote    string `json:"quote"`
-	Author   string `json:"author"`
 	Category string `json:"category"`
+	Author   string `json:"author"`
+	Message  string `json:"message,omitempty"`
+	Error    string `json:"error,omitempty"`
 }
 
-func processQuoteRequest() {
+type WSDictionaryResponse struct {
+	Request    string `json:"request"`
+	Definition string `json:"definition"`
+	Word       string `json:"word"`
+	Valid      bool   `json:"valid"`
+	Error      string `json:"error,omitempty"`
 }
 
-func processDictionaryRequest() {
-}
+func processQuoteRequest(wsRequest WSRequest) []WSQuoteResponse {
+	var quoteResponses []api.QuoteResponse
+	var quoteErr error
 
-func socketReader(wsConn *websocket.Conn) {
-	for {
-		// Read message from client
-		var clientRequest ClientRequest
-		readErr := wsConn.ReadJSON(&clientRequest)
-		if readErr != nil {
-			log.Print("Error reading from socket: ", readErr)
-			return
+	// Send request to Public API
+	var wsQuoteResponses []WSQuoteResponse
+	quoteErr, quoteResponses = api.QuoteRequest(wsRequest.Category, wsRequest.Limit)
+	if quoteErr != nil {
+		wsQuoteResponse := WSQuoteResponse{
+			Request: wsRequest.Request,
+			Error:   quoteErr.Error(),
 		}
 
-		log.Printf("%s received %s request from client\n", wsConn.RemoteAddr(), clientRequest.Request)
-
-		// Prcocess API request
-		var quoteResponses []api.QuoteResponse
-		var quoteErr error
-		var dictResponse *http.Response
-		var dictErr error
-		serverResponses := make([]ServerResponse, 0)
-
-		switch clientRequest.Request {
-		case "Quote":
-			quoteResponses, quoteErr = api.QuoteRequest(clientRequest.Category, clientRequest.Limit)
-			if quoteErr != nil {
-				log.Print("Error processing quote request: ", quoteErr)
-				return
+		wsQuoteResponses = append(wsQuoteResponses, wsQuoteResponse)
+	} else {
+		// Construct Websocket Response message
+		if len(quoteResponses) == 0 {
+			msg := "No items were returned"
+			wsQuoteResponse := WSQuoteResponse{
+				Request: wsRequest.Request,
+				Message: msg,
 			}
 
+			wsQuoteResponses = append(wsQuoteResponses, wsQuoteResponse)
+		} else {
 			for _, quoteResponse := range quoteResponses {
-				serverResponse := ServerResponse{
-					Request:  clientRequest.Request,
+				wsQuoteResponse := WSQuoteResponse{
+					Request:  wsRequest.Request,
 					Quote:    quoteResponse.Quote,
 					Author:   quoteResponse.Author,
 					Category: quoteResponse.Category,
 				}
 
-				serverResponses = append(serverResponses, serverResponse)
+				wsQuoteResponses = append(wsQuoteResponses, wsQuoteResponse)
 			}
+		}
+	}
 
-		case "Dictionary":
-			dictResponse, dictErr = api.DictionaryRequest(clientRequest.Word)
-			if dictErr != nil {
-				log.Print("Error processing quote request: ", quoteErr)
+	return wsQuoteResponses
+}
+
+func processDictionaryRequest(wsRequest WSRequest) *WSDictionaryResponse {
+	var dictResponse api.DictionaryResponse
+	var dictErr error
+
+	dictErr, dictResponse = api.DictionaryRequest(wsRequest.Word)
+
+	var wsDictResponse *WSDictionaryResponse
+	if dictErr != nil {
+		wsDictResponse = &WSDictionaryResponse{
+			Request: wsRequest.Request,
+			Error:   dictErr.Error(),
+		}
+
+	} else {
+		wsDictResponse = &WSDictionaryResponse{
+			Request:    wsRequest.Request,
+			Definition: dictResponse.Definition,
+			Word:       dictResponse.Word,
+			Valid:      dictResponse.Valid,
+		}
+	}
+
+	return wsDictResponse
+}
+
+func socketReader(wsConn *websocket.Conn) {
+	for {
+		// Read message from client
+		var wsRequest WSRequest
+		readErr := wsConn.ReadJSON(&wsRequest)
+		if readErr != nil {
+			log.Print("Error reading from socket: ", readErr)
+			return
+		}
+
+		log.Printf("%s received %s request from client\n", wsConn.RemoteAddr(), wsRequest.Request)
+
+		// Prcocess API request
+		switch wsRequest.Request {
+		case "Quote":
+			wsQuoteResponses := make([]WSQuoteResponse, 0)
+			wsQuoteResponses = processQuoteRequest(wsRequest)
+
+			// Send response to client
+			writeErr := wsConn.WriteJSON(wsQuoteResponses)
+			if writeErr != nil {
+				log.Print("Error writing to socket: ", writeErr)
 				return
 			}
 
-			if dictResponse == nil {
-				log.Print("Parsing error")
+		case "Dictionary":
+			wsDictResponse := processDictionaryRequest(wsRequest)
+
+			// Send response to client
+			writeErr := wsConn.WriteJSON(*wsDictResponse)
+			if writeErr != nil {
+				log.Print("Error writing to socket: ", writeErr)
 				return
 			}
 
 		default:
-			log.Print("Unhandled request: ", clientRequest.Request)
-		}
-
-		// Send response to client
-		// serverResponse.Request = clientRequest.Request
-		writeErr := wsConn.WriteJSON(serverResponses)
-		if writeErr != nil {
-			log.Print("Error writing to socket: ", writeErr)
-			return
+			log.Print("Unhandled request: ", wsRequest.Request)
 		}
 
 		log.Printf("%s sent client request response to client\n", wsConn.RemoteAddr())
